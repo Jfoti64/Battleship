62e60f269b86ea9c2450549efa3dcacd06a3a51d
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-21 21:41:15 -0500

    Create gameboard class

--- tests/ships.test.js
+++ tests/ships.test.js
@@ -1,8 +1,15 @@
-import Ship from '../src/ships.js'
+import Ship from '../src/ships.js';
 
-test('Increase hits until ship isSunk returns true', () => {
+test('Increase hits until ship isSunk returns correct value', () => {
   const length = 1;
   const ship = new Ship(length);
   ship.hit();
   expect(ship.isSunk()).toBe(true);
+});
+
+test("Check that ship dosn't sink until hit enough times", () => {
+  const length = 2;
+  const ship = new Ship(length);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
 });
